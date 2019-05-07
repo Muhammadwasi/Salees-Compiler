afb1060a9cfb8da00f222e1e50e8ade851c73486
muhammadwasi228 <muhammadwasi228@ymail.com>
2019-05-07 06:43:15 +0500

    Adding intermediate code generation file

---
+++ code_generator.py
@@ -0,0 +1,194 @@
+from lark import Lark,Transformer
+class ICGenerator(Transformer):
+
+    def __init__(self):
+        self.scope=10000
+        self.regCount=0
+        self.sym_table={}
+        self.ic=""
+        self.line_count=0
+
+    def top_level(self,items):
+        self.ic+=(items[0])
+
+    def getNewLine(self):
+        lc="L"+str(self.line_count)
+        self.line_count+=1
+        return lc
+
+    def getNewReg(self):
+        reg= "t"+str(self.regCount)
+        self.regCount+=1
+        return reg
+
+    def immutable(self,items):
+        return items[0]
+
+    def mutable(self,items):
+        if items[0] not in self.sym_table:
+            raise Exception("Variable {} is not declared".format(items[0]))
+        elif items[0] in self.sym_table and "value" not in self.sym_table[items[0]]:
+            raise Exception("Variable {} is not initialzed".format(items[0]))
+        return items[0]
+    
+    def mul_expression(self,items):
+        if len(items)>2:
+            reg=self.getNewReg()
+            if len(items[0])==2:
+                code=items[0][1]+"\n"+reg+"=" +items[0][0] +items[1] +items[2]
+            else:
+                code= "\n"+reg+"=" +items[0] +items[1] +items[2]
+            return [reg,code]
+        else:
+            return items[0]
+
+    def sum_expression(self,items):
+        if len(items)>2:
+            reg=self.getNewReg()
+            if len(items[0])==2 and len(items[2])==2:
+                code=items[0][1]+items[2][1]+"\n"+reg+"=" +items[0][0] +items[1] +items[2][0]
+
+            elif len(items[2])==2:
+                code=items[2][1]+"\n"+reg+"=" +items[0] +items[1] +items[2][0]
+            elif len(items[0])==2:
+                code=items[0][1]+"\n"+reg+"=" +items[0][0] +items[1] +items[2]
+            else:
+                code="\n"+reg+"=" +items[0] +items[1] +items[2]
+            return [reg,code]
+        else:
+            return items[0]
+
+    def relop(self,items):
+        return items[0]
+
+    def rel_expression(self,items):
+        if len(items)>2:
+            reg=self.getNewReg()
+            if len(items[0])==2 and len(items[2])==2:
+                code=items[0][1]+items[2][1]+"\n"+reg+"="+items[0][0]+items[1]+items[2][0]
+            elif len(items[0])==2:
+                code=items[0][1]+"\n"+reg+"="+items[0][0]+items[1]+items[2]
+            elif len(items[2])==2:
+                code=items[2][1]+"\n"+reg+"="+items[0]+items[1]+items[2][0]
+            else:
+                code="\n"+reg+"="+items[0]+items[1]+items[2]
+
+            return [reg,code]
+        else:
+            return items[0]
+
+    def unary_rel_expression(self,items):
+        if len(items)==2:
+            reg=self.getNewReg()
+            if(len(items[1])==2):
+                code=items[1][1]+"\n"+reg+"="+items[0]+items[1][0]
+            else:
+                code="\n"+reg+"="+items[0]+items[1]
+            return [reg,code]
+        else: 
+            return items[0]
+
+    def and_expression(self,items):
+        if len(items)>2:
+            reg=self.getNewReg()
+            if len(items[0])==2 and len(items[2])==2:
+                code=items[0][1]+items[2][1]+"\n"+reg+"="+items[0][0]+items[1]+items[2][0]
+            elif len(items[0])==2:
+                code=items[0][1]+"\n"+reg+"="+items[0][0]+items[1]+items[2]
+            elif len(items[2])==2:
+                code=items[2][1]+"\n"+reg+"="+items[0]+items[1]+items[2][0]
+            else:
+                code="\n"+reg+"="+items[0]+items[1]+items[2]
+
+            return [reg,code]
+        else:
+            return items[0]
+
+    def simple_expression(self,items):
+        if len(items)>2:
+            reg=self.getNewReg()
+            if len(items[0])==2 and len(items[2])==2:
+                code=items[0][1]+items[2][1]+"\n"+reg+"="+items[0][0]+items[1]+items[2][0]
+            elif len(items[0])==2:
+                code=items[0][1]+"\n"+reg+"="+items[0][0]+items[1]+items[2]
+            elif len(items[2])==2:
+                code=items[2][1]+"\n"+reg+"="+items[0]+items[1]+items[2][0]
+            else:
+                code="\n"+reg+"="+items[0]+items[1]+items[2]
+
+            return [reg,code]
+        else:
+            return items[0]
+
+    def expression(self, items):
+        return items[0]
+    
+    def factor(self,items):
+        return items[0]
+   
+    def statement(self,items):
+        return items[0]
+
+    def declaration(self,items):
+        return items[0]
+
+    def declare_init(self,items):
+        if items[1] in self.sym_table:
+            raise Exception("Variable {} is already declared of type {}".format(items[1],self.sym_table[items[1]]["type"]))
+        
+        if len(items[2])==2:            
+            code=items[2][1]+"\n"+items[1]+"="+items[2][0]
+            self.sym_table[items[1]]={"type":items[0],"value":items[2][0]}
+        else:
+            code="\n"+items[1]+"="+items[2]
+            self.sym_table[items[1]]={"type":items[0],"value":items[2]}
+
+        return code
+
+    def type_specifier(self,items):
+        return items[0]
+
+    def init(self,items):
+        if items[0] not in self.sym_table:
+            raise Exception("Variable {} is not declared".format(items[0]))
+        if len(items[1])==2:
+            code=items[1][1]+"\n"+items[0]+"="+items[1][0]
+
+        else:
+            code="\n"+items[0]+"="+items[1]
+        return code
+
+    def declare(self, items):
+        if items[1] in self.sym_table:
+            raise Exception("Variable {} is already declared of type {}".format(items[1],self.sym_table[items[1]]["type"]))
+        self.sym_table[items[1]]={"type":items[0]}
+        return ""
+
+    def statement(self,items):
+        if len(items)==2:
+            return items[0]+items[1]
+        else:
+            return items[0]
+    
+    def selection_statement(self,items):
+        lc=self.getNewLine()
+
+        if len(items)==3:
+            if items[0]=="if":
+                code=items[1][1]+"\n"+"IFZ " +items[1][0]+ " GOTO "+lc+items[2]+"\n"+lc+":"
+        elif len(items)==5:
+            lc1=self.getNewLine()
+            code=items[1][1]+"\n"+"IFZ " +items[1][0]+ " GOTO "+lc+items[2]+"\nGoto "+lc1+"\n"+lc+":"+items[4]+"\n"+lc1+":"
+
+        return code
+
+    def iteration_statement(self,items):
+        lc=self.getNewLine()
+        lc1=self.getNewLine()
+        if len(items)==3:
+            code="\n"+lc+":"+items[1][1]+"\nIFZ "+items[1][0]+" Goto "+lc1+items[2]+"\nGoto "+lc+"\n"+lc1+":"
+        return code
+
+    def compound_statement(self,items):
+        if len(items)==1:
+            return items[0]
---
+++ salees_parser.py
@@ -0,0 +1,120 @@
+from lark import Lark,Transformer 
+from code_generator import ICGenerator
+class SaleesParser:
+    def __init__(self, programText,starter):
+        self.programText=programText
+        self.starter=starter
+        self.grammar=None
+        self.parseTree=None
+        self.initializeGrammar()
+
+    def initializeGrammar(self):
+        self.grammar=Lark(r"""
+        
+    top_level: statement
+
+    statement : (compound_statement
+        | selection_statement
+        | iteration_statement
+        | jump_statement
+        | declaration) [statement]
+
+    declaration: declare_init | declare | init
+    declare_init: type_specifier IDENTIFIER "=" expression ";"
+    declare : type_specifier IDENTIFIER ";"     
+    init : IDENTIFIER "=" expression ";"
+
+    compound_statement: "{" "}" 
+        | "{" statement "}"
+
+    selection_statement : IF "(" expression ")" compound_statement
+        | IF "(" expression ")" compound_statement OTHER compound_statement
+        
+    iteration_statement : LOOPTILL "(" expression ")" compound_statement
+        | DO compound_statement LOOPTILL "(" expression ")" ";"
+        
+    jump_statement : CONTINUE ";"
+        | BREAK ";"
+        | RETURN ";"
+        | RETURN expression ";"
+
+
+    expression: simple_expression
+    simple_expression: simple_expression OR_OP and_expression | and_expression
+    and_expression: and_expression AND_OP unary_rel_expression | unary_rel_expression
+    unary_rel_expression: NOT_OP unary_rel_expression | rel_expression
+    rel_expression : sum_expression relop sum_expression | sum_expression
+    sum_expression: sum_expression SUM_OP mul_expression | mul_expression
+    mul_expression : mul_expression MUL_OP factor | factor
+    factor : mutable | immutable
+    immutable:  WORD_CONST | WHOLE_CONST | FRAC_CONST | LETTER_CONST | BOOL_CONST 
+    mutable: IDENTIFIER
+
+    relop : LE_OP | GE_OP | EQ_OP | NE_OP | L_OP | G_OP
+    
+    type_specifier:LETTER
+        | FRAC
+        | WHOLE
+        | WORD
+        | BOOL
+        
+        L_OP: "<"
+        G_OP: ">"
+        SUM_OP: "+" | "-"
+        MUL_OP: "*" | "/" | "%"
+        NOT_OP: "~"
+
+        LOOP: "loop"
+        LOOPTILL: "looptill"
+        DO: "do"
+        IF: "if"
+        OTHER: "other"
+        CONTINUE: "continue"
+        BREAK: "break"
+
+        
+        IDENTIFIER : /[a-zA-Z_][a-zA-Z0-9_]*/
+
+        WORD_CONST : ESCAPED_STRING 
+        WHOLE_CONST: /-?\d+/
+        FRAC_CONST: /-?\d.\d+/
+        LETTER_CONST : "\'" "a".."z" "\'"
+        BOOL_CONST: "true" | "false"
+
+        AND_OP : "&&"
+        OR_OP : "||"
+        LE_OP : "<="
+        GE_OP : ">="
+        EQ_OP : "=="
+        NE_OP : "!="
+
+        LETTER : "letter"
+        FRAC : "frac"
+        WHOLE : "whole"
+        WORD : "word"
+        BOOL : "bool"
+        RETURN : "return"
+
+        %import common.ESCAPED_STRING
+        %import common.SIGNED_NUMBER
+        %import common.WS
+        %ignore WS 
+        """,start=self.starter)
+
+    def parserProgram(self):
+        self.parseTree=self.grammar.parse(self.programText)
+    
+    def showParseTree(self,isPretty):
+        if isPretty:
+            print self.parseTree.pretty()
+        else:
+            print self.parseTree
+    
+    def generateIC(self,fileName=None):
+        icg=ICGenerator()
+        icg.transform(self.parseTree)
+        if fileName:
+            with open(fileName,"w+") as f:
+                f.write(icg.ic)
+        return icg.ic
+    
